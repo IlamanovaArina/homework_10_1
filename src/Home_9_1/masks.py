c7d83e34dbc6cbc531c10ecdf119ab5fec671715
Arina <ilamanova.arina@gmail.com>
2024-09-07 02:19:45 +0300

    тесты к masks

--- src/Home_9_1/masks.py
+++ src/Home_9_1/masks.py
@@ -1,4 +1,5 @@
 from typing import Union
+from src.data import numbers
 
 def get_mask_card_number(card_number: Union[str]) -> Union[str]:
     """
@@ -7,7 +8,12 @@ def get_mask_card_number(card_number: Union[str]) -> Union[str]:
     """
     list_digit: Union[list] = []
 
+    if type(card_number) is not str or type(card_number) is bool:
+        return ""
+
     for key, value in enumerate(card_number):
+        if value not in numbers:
+            return ""
         if key % 4 == 0 and key != 0:
             list_digit.append(" ")
         if len(list_digit) <= 6 or len(list_digit) > 13:
@@ -27,10 +33,23 @@ def get_mask_account(card_number: Union[str]) -> Union[str]:
      и возвращает маску ввиде две '*' и 4 цифры
     """
     list_digit: Union[list] = []
+    intake = ""
+
+    if type(card_number) is not int:
+        if len(card_number) < 20:
+            return "Ошибка"
+    if type(card_number) is not str or type(card_number) is bool:
+        return "Ошибка"
 
     for digit in card_number:
-        list_digit += digit
-    list_digit[:-4] = '**'
+        if digit in numbers:
+            list_digit += digit
+            intake = "yes"
+
+    if intake == "yes":
+        list_digit[:-4] = '**'
+    else:
+        return "Ошибка"
 
     bank_card_str = "".join(list_digit)
     return bank_card_str
