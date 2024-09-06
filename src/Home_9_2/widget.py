7feaa8f31584ecfc0e566d641b855d17cc544d4b
Arina <ilamanova.arina@gmail.com>
2024-09-06 22:56:05 +0300

    первая функция виджета и тесты к ней

--- src/Home_9_2/widget.py
+++ src/Home_9_2/widget.py
@@ -1,32 +1,48 @@
 from typing import Union
+from src.Home_9_1.masks import get_mask_account, get_mask_card_number
+from src.data import acceptable_data, numbers
+import pytest
 
-def mask_account_card(bank_card: Union[str]) -> Union[str]:
+def mask_account_card(bank_card):
     """
     Функция которая принимает данные карты или счёта
     использует импортируемую функцию
     и возвращает в виде строки тип карты или счёта и
     маскированный номер карты/счёта
     """
-    numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
     account_or_number = []
     list_characters:Union[list] = []
+    entered_data = []
 
-    for symbol in bank_card:  # Перебераем каждый символ
+    if not hasattr(bank_card, '__iter__'):
+        return ""
+    if bank_card == [] or bank_card == {} or bank_card == ():
+        return ""
+
+    for symbols in bank_card:
+        entered_data.append(symbols)
+
+    entered_data_str = "".join(entered_data)
+
+    for symbol in entered_data_str:  # Перебераем каждый символ
         if symbol in numbers:
             list_characters += symbol  # Складываем цифы
         else:
             account_or_number.append(symbol)  # Складываем буквы
 
-    type_card_or_account = "".join(account_or_number)  # Собираем слово
+    type_card_or_account = "".join(account_or_number[:-1])  # Собираем слово
 
-    if type_card_or_account != "Счет ":
-        # Маскирует полученный номер карты
-        mask_card = get_mask_card_number("".join(list_characters))
-        return type_card_or_account + mask_card
-    elif type_card_or_account == "Счет ":
-        # Маскируем полученный "Счёт"
-        mask_card = get_mask_account("".join(list_characters))
-        return type_card_or_account + mask_card
+    if type_card_or_account in acceptable_data:
+        if type_card_or_account != "Счет":
+            # Маскирует полученный номер карты
+            mask_card = get_mask_card_number("".join(list_characters))
+            return type_card_or_account + " " + mask_card
+        elif type_card_or_account == "Счет":
+            # Маскируем полученный "Счёт"
+            mask_card = get_mask_account("".join(list_characters))
+            return type_card_or_account + " " + mask_card
+    else:
+        return ""
 
 
 def get_date(dat):
@@ -35,4 +51,3 @@ def get_date(dat):
     такого образца"2024-03-11T02:26:18.671407"
     """
     return f"{dat[8:10]}.{dat[5:7]}.{dat[0:4]} "
-
---
+++ tests/test_widget.py
@@ -0,0 +1,17 @@
+from src.Home_9_2.widget import mask_account_card, get_date
+import pytest
+
+def test_widget_mask_account_card():
+    assert mask_account_card("Visa Platinum 7000792289606361") == "Visa Platinum 7000 79** **** 6361"
+    assert mask_account_card('Счет 73654108430135874305') == "Счет **4305"
+    assert mask_account_card(700079223) == ""
+    assert mask_account_card([]) == ""
+    assert mask_account_card({}) == ""
+    assert mask_account_card("а если буквы, символы?") == ""
+    assert mask_account_card("а если 1 -буквы, 2 -символы и 3 -цыфры?") == ""
+    with pytest.raises(TypeError):
+        assert mask_account_card([7, 0, 0, 0, 7, 9, 2, 2, 3]) == "7000 79** *"
+        assert mask_account_card() == ""
+
+def test_widget_get_date():
+    assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024 "
