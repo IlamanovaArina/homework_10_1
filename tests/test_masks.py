d6bb3e874bdc2ce3d2d8c35b0e238725a05d3f52
Arina <ilamanova.arina@gmail.com>
2024-09-08 10:42:00 +0300

    ещё раз перенесла зачем-то

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -6,7 +6,8 @@ from src.Home_9_1.masks import get_mask_account, get_mask_card_number
 def test_masks():
     assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
     assert get_mask_card_number("7000") == "7000"
-    assert get_mask_card_number("7000123456789123456") == "7000 12** **** 9123 456"
+    assert (get_mask_card_number("7000123456789123456") ==
+            "7000 12** **** 9123 456")
     assert get_mask_card_number("пробуем буквы") == ""
     assert get_mask_card_number("") == ""
     assert get_mask_card_number(7000792289606361) == ""
