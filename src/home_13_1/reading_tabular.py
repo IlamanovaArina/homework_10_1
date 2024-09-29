a2b54c95acfcde4aefd2555800022c0019f382b8
Arina <ilamanova.arina@gmail.com>
2024-09-29 16:30:57 +0300

    Исправила тесты

--- src/home_13_1/reading_tabular.py
+++ src/home_13_1/reading_tabular.py
@@ -7,21 +7,29 @@ logger_csv = logging.getLogger("transaction_csv.log")
 logger_excel = logging.getLogger("transaction_excel.log")
 logger_csv.setLevel(logging.DEBUG)
 logger_excel.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename=r"C:\Users\minac.DESKTOP-L51PJSH\PycharmProjects"
-                              r"\homework_10_1\src\home_13_1\logs.log",
-                              encoding='utf-8')
-formatter = logging.Formatter('%(asctime)s %(filename)s'
-                              '%(funcName)s %(levelname)s - %(message)s')
+handler = logging.FileHandler(
+    filename=r"C:\Users\minac.DESKTOP-L51PJSH\PycharmProjects"
+    r"\homework_10_1\src\home_13_1\logs.log",
+    encoding="utf-8",
+)
+formatter = logging.Formatter(
+    "%(asctime)s %(filename)s" "%(funcName)s %(levelname)s - %(message)s"
+)
 handler.setFormatter(formatter)
 
 logger_csv.addHandler(handler)
 logger_excel.addHandler(handler)
 
 
-file_path_csv_ = (r"C:\Users\minac.DESKTOP-L51PJSH\PycharmProjects"
-                  r"\homework_10_1\data\transactions.csv")
-file_path_xlsx_ = (r"C:\Users\minac.DESKTOP-L51PJSH\PycharmProjects"
-                   r"\homework_10_1\data\transactions_excel.xlsx")
+file_path_csv_ = (
+    r"C:\Users\minac.DESKTOP-L51PJSH\PycharmProjects"
+    r"\homework_10_1\data\transactions.csv"
+)
+file_path_xlsx_ = (
+    r"C:\Users\minac.DESKTOP-L51PJSH\PycharmProjects"
+    r"\homework_10_1\data\transactions_excel.xlsx"
+)
+
 
 def reading_tables_csv(file_path_csv) -> list:
     """Функция которая открывает и читает файлы .csv
@@ -31,7 +39,7 @@ def reading_tables_csv(file_path_csv) -> list:
         list_row = []
         with open(file_path_csv, encoding="utf-8") as csv_file:
             logger_csv.info(f"Открыт файл: {file_path_csv}")
-            reader = csv.DictReader(csv_file, delimiter=';')
+            reader = csv.DictReader(csv_file, delimiter=";")
             for row in reader:
                 list_row.append(row)
 
@@ -52,7 +60,7 @@ def reading_tables_xlsx(file_path_xlsx) -> list:
 
         logger_excel.info(f"Открыли файл{file_path_xlsx}")
 
-        transaction_excel = df.to_dict(orient='records')
+        transaction_excel = df.to_dict(orient="records")
 
         logger_excel.debug("Преобразовали данные из Exsel в список словарей")
 
--- tests/test_reading_tabuiar.py
+++ tests/test_reading_tabuiar.py
@@ -3,51 +3,46 @@ from unittest.mock import patch, mock_open
 from src.home_13_1.reading_tabular import (reading_tables_csv,
                                            reading_tables_xlsx)
 
+file_path_csv_ = (
+    r"C:\Users\minac.DESKTOP-L51PJSH\PycharmProjects"
+    r"\homework_10_1\data\transactions.csv"
+)
+file_path_xlsx_ = (
+    r"C:\Users\minac.DESKTOP-L51PJSH\PycharmProjects"
+    r"\homework_10_1\data\transactions_excel.xlsx"
+)
 
-file_path_csv_ = (r"C:\Users\minac.DESKTOP-L51PJSH\PycharmProjects"
-                  r"\homework_10_1\data\transactions.csv")
-file_path_xlsx_ = (r"C:\Users\minac.DESKTOP-L51PJSH\PycharmProjects"
-                   r"\homework_10_1\data\transactions_excel.xlsx")
 
-
-@patch("os.path.exists")
 @patch("csv.DictReader")
-def test_tabular_csv(mocked_dict, mock_csv_exists):
+def test_tabular_csv(example_csv_reader):
     """Тест на tabular_csv"""
-    mock_csv_exists.return_value = True
 
-    data = [
-        {"column1": "value1", "column2": "value2"},
-        {"column1": "value3", "column2": "value4"}
-    ]
+    example_csv_reader.return_value = [{
+        "column1": "value1",
+        "column2": "value2"
+    }]
 
-    reader_data = mock_open(read_data="\n".join(
-        [";".join(row.values()) for row in data]))
+    with patch("builtins.open", mock_open()) as file_mock_open:
+        assert reading_tables_csv("random_test_file.csv") == [
+            {"column1": "value1", "column2": "value2"}
+        ]
 
-    with (patch("builtins.open", reader_data),
-          patch("csv.DictReader")):
-        mocked_dict.return_value = data
+        file_mock_open.assert_called()
+        example_csv_reader.assert_called_with(file_mock_open(), delimiter=";")
 
-    assert reading_tables_csv(file_path_csv_) == data
-
-    # Это почемуто не работает, я пробовала и полный путь, всё равно ошибка
-
-    # mocked_open.assert_called_with(file_path_csv_)
-
-    # mocked_open.assert_called_with(
-    # r"C:\Users\minac.DESKTOP-L51PJSH\PycharmProjects
-    # \homework_10_1\data\transactions.csv", encoding="utf-8")
+        # example_csv_reader = < MagicMock name = 'DictReader'
+        # id = '2144005849552' >
 
 
 @patch("pandas.read_excel")
 def test_reading_tables_xlsx(mock_read_excel):
     file_path_xlsx = "test.xlsx"
-    expected_result = [{'c1': 'v1', 'c2': 'v2'}, {'c1': 'v3', 'c2': 'v4'}]
+    expected_result = [{"c1": "v1", "c2": "v2"}, {"c1": "v3", "c2": "v4"}]
 
     # Моделируем чтение файла Excel и имитируем результат
     mock_read_excel.return_value.to_dict.return_value = expected_result
 
-    result = reading_tables_xlsx(file_path_xlsx)
+    assert reading_tables_xlsx(file_path_xlsx) == expected_result
+    mock_read_excel.assert_called_with(file_path_xlsx)
 
-    assert result == expected_result
-    # .assert_called_with(file_path_xlsx_)
+    # mock_read_excel = < MagicMock name = 'read_excel' id = '1517065600800' >
